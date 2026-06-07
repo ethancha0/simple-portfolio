4c578213ac4620bdaab3a585cf83ca7c0ed04595
ethancha0 <ethanchao2005@gmail.com>
2026-06-07 16:12:19 -0700

    feat: times new roman

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1 className="text-4xl text-white  flex justify-center items-center h-screen ">Hello World - Ethan Chao</h1>
     </main>
   );
 }
