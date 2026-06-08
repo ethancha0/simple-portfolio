39f41670907bfd9b801b435770f6743be4a93208
ethancha0 <ethanchao2005@gmail.com>
2026-06-08 10:17:12 -0700

    feat: ascii bonsai

--- components/body.tsx
+++ components/body.tsx
@@ -1,30 +1,36 @@
 import TextType from "./animations/TextType";
+import { BONSAI_ART } from "./bonsai-art";
 
 export default function Body(){
 
     return(
-        <div className="flex flex-col justify-start">
+        <div className="flex justify-center items-center">
+
+            <div className="flex flex-col justify-start">
+                <TextType 
+                className="text-4xl text-white mb-8"
+                loop={false}
+                typingSpeed={50}
+                pauseDuration={4200}
+                showCursor
+                cursorCharacter="|"
+                text="> Hi, I'm Ethan "
+                cursorBlinkDuration={1}
+                />
+
+                <div className="max-w-80">
+                
+                    <p className="text-2xl">
+                        Currently leading a team to build <a href="https://zotmeet.com/" target="_blank" className="underline">ZotMeet</a> for 
+                        UC Irvine students. There, I also study Software Engineering & Health Infomatics
+                    </p>
 
 
-            <TextType 
-            className="text-4xl text-white mb-8"
-            loop={false}
-            typingSpeed={50}
-            pauseDuration={4200}
-            showCursor
-            cursorCharacter="|"
-            text="> Hi, I'm Ethan "
-            cursorBlinkDuration={1}
-            />
+                </div>
+            </div>
 
-            <div className="max-w-80">
-            
-                <p className="text-2xl">
-                    Currently leading a team to build <a href="https://zotmeet.com/" target="_blank" className="underline">ZotMeet</a> for 
-                    UC Irvine students. There, I also study Software Engineering & Health Infomatics
-                </p>
-
-
+            <div>
+                <pre className="overflow-x-auto whitespace-pre font-[ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,monospace] text-[9px] leading-[1]">{BONSAI_ART}</pre>
             </div>
 
 
