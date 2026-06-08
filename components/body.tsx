1ba98289fa867454a06ffc434e49a9d187acd2bf
ethancha0 <ethanchao2005@gmail.com>
2026-06-08 10:06:08 -0700

    feat: body

--- components/body.tsx
+++ components/body.tsx
@@ -3,11 +3,11 @@ import TextType from "./animations/TextType";
 export default function Body(){
 
     return(
-        <div>
+        <div className="flex flex-col justify-start">
 
 
             <TextType 
-            className="text-4xl text-white"
+            className="text-4xl text-white mb-8"
             loop={false}
             typingSpeed={50}
             pauseDuration={4200}
@@ -16,6 +16,22 @@ export default function Body(){
             text="> Hi, I'm Ethan "
             cursorBlinkDuration={1}
             />
+
+            <div className="max-w-80">
+            
+                <p className="text-2xl">
+                    Currently leading a team to build <a href="https://zotmeet.com/" target="_blank" className="underline">ZotMeet</a> for 
+                    UC Irvine students. There, I also study Software Engineering & Health Infomatics
+                </p>
+
+
+            </div>
+
+
+
+
+
+
 
         </div>
 
